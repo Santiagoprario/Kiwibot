c02e22258cdc6e2fa16319bf5a0bb7660db43539
Bob <bob@example.com>
2021-11-26 14:14:42 -0300

    router & order

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './Components/Home';
+import Add from './Components/Adds';
+import { Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Route exact path='/add' component={Add}  />
+      <Route exact path='/'    component={Home} />
     </div>
   );
 }
